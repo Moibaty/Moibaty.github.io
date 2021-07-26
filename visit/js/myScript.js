c5a2d528a94c057a75c90405158330c405b993b4
Moibaty <86291695+Moibaty@users.noreply.github.com>
2021-07-26 16:50:47 +0300

    Начал изучение php

---
+++ visit/js/myScript.js
@@ -0,0 +1,262 @@
+"use strict"
+
+
+var array = [];
+
+/*var l = prompt("ТИП САЙТА :\n 1-Интернет-магазин(30.000руб.)\n 2-Сайт-визитка(10.000руб.)\n 3-Корпоративные сайты(50.000руб.)");
+for (var h = Number(l); h != 1 && h != 2 && h != 3;) {
+    alert("Недопустимое значение.\n Напишите номер желаемого ответа!");
+    h = prompt("ТИП САЙТА :\n 1-Интернет-магазин(30.000руб.)\n 2-Сайт-визитка(10.000руб.)\n 3-Корпоративные сайты(50.000руб.)");
+};
+h = Number(h);
+l = h;
+array[0] = l;
+
+var w = prompt("ДИЗАЙН САЙТА:\n 1-Индивидуальный(±5.000руб.) \n 2-Шаблонный(±1.000руб.) (Увидеть примеры вы можете в разделе Кейсы)");
+for ( var s = Number(w); s != 1 && s != 2;) {
+    alert("Недопустимое значение.\n Напишите номер желаемого ответа!");
+    s = prompt("ДИЗАЙН САЙТА:\n 1-Индивидуальный(±5.000руб.) \n 2-Шаблонный(±1.000руб.) (Увидеть примеры вы можете в разделе Кейсы)");
+};
+s = Number(s);
+w = s;
+array[1] = w;
+
+var i = prompt("АДАПТИВНОСТЬ:\n 1-Только мобильные устройства(3.000руб.)(375px-991px) \n 2-Только широкоформатные экраны(2.000руб.)(≥992px)\n 3-Все устройства(4.000руб.) (≥375px)");
+for (var k = Number(i); k != 1 && k != 2 && k != 3;) {
+    alert("Недопустимое значение.\n Напишите номер желаемого ответа!");
+    k = prompt("АДАПТИВНОСТЬ:\n 1-Только мобильные устройства(3.000руб.)(375px-991px) \n 2-Только широкоформатные экраны(2.000руб.)(≥992px)\n 3-Все устройства(4.000руб.) (≥375px)");
+};
+k = Number(k);
+i = k;
+array[2] = i;
+console.log(array);
+var c = 0;
+var t = 0;
+if(array[0] == 1) {
+    c = 30000;
+    t = 14;
+};
+if(array[0] == 2) {
+    c = 10000;
+    t = 7;
+};
+if(array[0] == 3) {
+    c = 50000;
+    t = 21;
+};
+
+if(array[1] == 1) {
+    c +=5000;
+     t += 2;
+};
+if(array[1] == 2) {
+    c +=1000;
+    t += 1;
+};
+if(array[2] == 1) {
+    c += 3000;
+    t += 2;
+};
+if(array[2] == 2) {
+    c += 2000;
+    t += 2;
+};
+if(array[2] == 3) {
+    c += 4000;
+    t += 4;
+};
+console.log(typeof c);
+console.log(c);
+var o = "Цена выбранных вами услуг составлят:" + c +"\n Если вы готовы сделать заказ, то обратитесь по этому номеру телефона : +7 999 999 99 99";
+confirm(o);
+t = String(t);
+console.log(typeof t);
+console.log(t);
+var p = t + " сут.";
+var f = c + " руб.";
+document.getElementById("Sroki").textContent= p;
+document.getElementById("Cena").textContent= f;*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function () {
+    let options = {
+        threshold: [0.5]
+    };
+    let observer = new IntersectionObserver(onEntry, options);
+    let elements = $('#ananas');
+    elements.each((i, el) => {
+        observer.observe(el);
+    });
+
+
+
+
+});
+
+function onEntry(entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            $('.animationnumbers').each(function () {
+                $(this).prop('Counter', 0).animate({
+                    Counter: $(this).text()
+                }, {
+                    duration: 4000,
+                    easing: 'swing',
+                    step: function (now) {
+                        $(this).text(Math.ceil(now));
+                    }
+                });
+                
+            });
+
+        };
+    });
+}; 
+
+
+
+
+
+$('.js-close-campaign').click(function() {
+    $('.js-overlay-campaign').fadeOut();
+    $('main').css('filter','none');
+});
+
+$(window).on('load', function() {
+    setTimeout(function(){
+        if($('js-overlay-campaign').hasClass('disabled')) {
+            return false;
+        }
+        else {
+            $('main').css('filter','blur(5px)');
+            $(".js-overlay-campaign").fadeIn();
+        }
+    }, 10000);
+});
+
+
+
+
+
+
+
+
+
+
+$('a[href^="#"]').click(function() {
+    let valHref = $(this).attr("href");
+    $('html,body').animate({scrollTop: $(valHref).offset().top -20 + "px"});
+});
+$(window).scroll(() => {
+    let scrollDistance = $(window).scrollTop();
+    $(".section").each((i,el) =>{
+       if($(el).offset().top - $("nav").outerHeight() <= scrollDistance) {
+           $("nav a").each((i,el) => {
+               if($(el).hasClass("active")) {
+                   $(el).removeClass("active");
+               }
+           });
+           $('nav li:eq(' + i + ')').find('a').addClass('active');
+       }
+    });
+}); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function calc() { 
+var select = document.getElementById("SiteType");
+var value = select.value;
+var c = Number(value);
+array[0] = c;
+var select1 = document.getElementById("Design");
+var value1 = select1.value;
+var d = Number(value1);
+array[1] = d;
+var select2 = document.getElementById("adaptability");
+var value2 = select2.value;
+var k = Number(value2);
+array[2] = k;
+
+var p = 0;
+var t = 0;
+if(array[0] == 1) {
+    p = 30000;
+    t = 14;
+};
+if(array[0] == 2) {
+    p = 10000;
+    t = 7;
+};
+if(array[0] == 3) {
+    p = 50000;
+    t = 21;
+};
+
+if(array[1] == 1) {
+    p +=5000;
+     t += 2;
+};
+if(array[1] == 2) {
+    p +=1000;
+    t += 1;
+};
+if(array[2] == 1) {
+    p += 3000;
+    t += 2;
+};
+if(array[2] == 2) {
+    p += 2000;
+    t += 2;
+};
+if(array[2] == 3) {
+    p += 4000;
+    t += 4;
+};
+
+t = String(t);
+p = String(p)
+var m = t + " сут.";
+var f = p + " руб.";
+document.getElementById("Sroki").textContent= m;
+document.getElementById("Cena").textContent= f;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ visit/php/mail.php
@@ -0,0 +1,37 @@
+<?php 
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$form= $_POST['form_price'];
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'gichev@internet.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'Artur18052005'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('gichev@internet.ru'); // от кого будет уходить письмо?
+$mail->addAddress('artursharif2005@icloud.com');     // Кому будет уходить письмо 
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка с тестового сайта';
+$mail->Body    = '' .$form . ' оставил заявку!';
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo false;
+} else {
+    echo true;
+}
+?>
